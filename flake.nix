fb0c181b7eb4800e507691c6aab0465f1caa8a44
Yifei Sun <ysun@hey.com>
2024-07-15 23:04:48 +0900

    treewide: remove flake compat

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    compat.url = "github:edolstra/flake-compat";
-    compat.flake = false;
     parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
   };
